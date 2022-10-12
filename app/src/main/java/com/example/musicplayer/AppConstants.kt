019e93a69267e5132f4ede126374c3f5e749537b
khushpajwani2 <79623464+khushpajwani2@users.noreply.github.com>
2022-10-12 12:03:28 +0530

    Revert "DONE:"

--- app/src/main/java/com/example/musicplayer/AppConstants.kt
+++ app/src/main/java/com/example/musicplayer/AppConstants.kt
@@ -8,13 +8,5 @@ open class AppConstants {
     companion object {
         var CHANNEL_ID = "1001"
         var NOTIFICATION_ID = 0
-        var MAIN_ACTION = "action.main"
-        var INIT_ACTION = "action.init"
-        var PREV_ACTION = "action.prev"
-        var PLAY_ACTION = "action.play"
-        var PAUSE_ACTION = "action.pause"
-        var NEXT_ACTION = "action.next"
-        var STARTFOREGROUND_ACTION = "action.startforeground"
-        var STOPFOREGROUND_ACTION = "action.stopforeground"
     }
 }
